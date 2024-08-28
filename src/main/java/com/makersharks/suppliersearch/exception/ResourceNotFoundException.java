bc0c0a3c7778e0576ff94d87d6205d6404c4def7
AfshanAlamEngg <afshanalamengg@gmail.com>
2024-08-28 15:20:32 +0530

    Sprint 7: Removed the project files because of a major issue encountered during ./gradlew build.

--- src/main/java/com/makersharks/suppliersearch/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.makersharks.suppliersearch.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
